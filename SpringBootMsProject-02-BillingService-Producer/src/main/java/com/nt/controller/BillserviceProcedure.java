9259482855130e3d3b468c082cd69ef38847dde5
user <user@TapasRout>
2023-05-03 06:49:46 +0530

    Log msg updated

--- SpringBootMsProject-02-BillingService-Producer/src/main/java/com/nt/controller/BillserviceProcedure.java
+++ SpringBootMsProject-02-BillingService-Producer/src/main/java/com/nt/controller/BillserviceProcedure.java
@@ -1,5 +1,7 @@
 package com.nt.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/producer")
 public class BillserviceProcedure {
+
+private static final Logger log = LoggerFactory.getLogger(BillserviceProcedure.class);
 
 	@Value("${server.port}")
 	private int port;
@@ -18,6 +22,7 @@ public class BillserviceProcedure {
 	private String id;
 	@GetMapping("/bill")
 	public String showMsg() {
+		log.info("showMsg-Method Executed");
 		return "Bill service Name is -- "+serviceName+" Port is"+port+" Id is --"+id;
 	}
 }//end of class BillserviceProcedure
--- SpringBootMsProject-02-ShopingConsumer-LBC/src/main/java/com/nt/lbc/BillserviceConsumer.java
+++ SpringBootMsProject-02-ShopingConsumer-LBC/src/main/java/com/nt/lbc/BillserviceConsumer.java
@@ -1,5 +1,7 @@
 package com.nt.lbc;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
@@ -9,14 +11,19 @@ import org.springframework.web.client.RestTemplate;
 @Component
 public class BillserviceConsumer {
 	
+	private static final Logger log = LoggerFactory.getLogger(BillserviceConsumer.class);
+
 	@Autowired
 	private LoadBalancerClient client;
 
 	public String getBillInfo() {
+		log.info("getBillInfo()-method start executing");
 		ServiceInstance choose = client.choose("Billing-Service"); //get service instance object
 		String url = choose.getUri()+"/producer/bill"; //make URl for producer app
 		//create a rest template object to send url request to producer app by using resttemplate
 		RestTemplate temp = new RestTemplate();
+		log.info("Rest template created sucessfully");
 		return temp.getForObject(url, String.class);
+	
 	}
 }//end of the class LoadbalancerClient
