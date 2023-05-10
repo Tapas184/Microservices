b749cb45cfd37b017f15b6856254e6803addca4f
user <user@TapasRout>
2023-05-10 16:42:56 +0530

    PTP Model Of JMS with scheduling

--- SpringBootMSProject-07-Message-JMS-PTP-Producer/src/main/java/com/nt/MessageRunner.java
+++ SpringBootMSProject-07-Message-JMS-PTP-Producer/src/main/java/com/nt/MessageRunner.java
@@ -1,19 +1,21 @@
-package com.nt.runner;
+package com.nt;
 
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
-public class MessageRunner implements CommandLineRunner {
+public class MessageRunner  {
 
 	@Autowired
 	private JmsTemplate template;
-	@Override
-	public void run(String... args) throws Exception {
+	
+	@Scheduled(cron = "*/5 * * * * * ")
+	public void send(){
 
 		template.send("PTP-MSG", ses->
 			ses.createTextMessage("PTP Message From Producer APP::"+new Date()));
